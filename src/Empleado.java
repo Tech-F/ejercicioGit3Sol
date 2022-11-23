75a97e01a52425c9fab0abe0bab77533cef8d241
Francisco Míguez Portela <a19franciscomp@ATEIS.LOCAL>
2022-11-23 19:26:39 +0100

    bug corregido y testeado

---
+++ src/Empleado.java
@@ -0,0 +1,15 @@
+    class Empleado {
+        final String nombre; // Ahora el campo nombre es inmutable.
+        int edad;
+
+        Empleado(String name, int edad) {
+            nombre = name;
+            this.edad = edad;
+        }
+
+        @Override
+        public String toString() {
+            return "El empleado " + nombre + " tiene" + edad + "años";
+        }
+
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Empleado p = new Empleado("Pepe Pérez", 30);
+        System.out.println(p);
+
+/*
+        p.nombre = "Otro nombre";  // Si intentamos cambiar el campo nombre, nos da un error de compilación
+        System.out.println(p);
+*/
+
     }
+
 }
